254fbe60afa255d3c07914ff024f89c7d3819d37
Ra5c0 <og2mil@gmail.com>
2024-06-15 20:48:51 +0200

    adding run_main.bat

---
+++ run_main.bat
@@ -0,0 +1,8 @@
+@echo off
+REM Ce fichier batch exécute main.py avec Python 3.9
+
+REM Définition du chemin vers Python 3.9
+set PYTHON_EXE=C:\Users\og2mi\AppData\Local\Programs\Python\Python39\python.exe
+
+REM Commande pour exécuter le script Python
+"%PYTHON_EXE%" main.py
